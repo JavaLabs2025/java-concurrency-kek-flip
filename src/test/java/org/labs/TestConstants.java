9df1c0b898375ab82bcc295471e28d27df51bfb4
Максим Давыдов <maxim.davydov@vkteam.ru>
2025-10-04 15:55:52 +0300

    Покрыл тестами Restaurant

---
+++ src/test/java/org/labs/TestConstants.java
@@ -0,0 +1,13 @@
+package org.labs;
+
+public class TestConstants {
+    public static final Integer MAX_EATING_TIME_MS = 1;
+    public static final Integer MAX_SERVER_TIME_MS = 1;
+
+    public static final Integer PORTIONS_COUNT = 100;
+    public static final Integer PROGRAMMERS_COUNT = 7;
+    public static final Integer WAITERS_COUNT = 2;
+
+    public static final Float AVERAGE_DELTA_PERCENT = 0.1f;
+    public static final Float DIFF_PERCENT = 0.25f;
+}
